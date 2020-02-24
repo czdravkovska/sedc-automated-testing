40c422c67f60c0e0d7aa9a679418cd496ef321bf
Cveta Zdravkovska <czdravkovska@gmail.com>
2020-02-25 00:02:26 +0100

    First homework

---
+++ Homework1/src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Task1();
+        Task2();
+    }
+
+    private static void Task1() {
+        int i;
+        i = 5;
+        int j;
+        j = 10;
+        System.out.println(i);
+        System.out.println(j);
+    }
+
+    private static void Task2() {
+        int i;
+        i = 10;
+        int j;
+        j = 5;
+        double mult = i * j;
+        String result = String.format("i=%d,j=%d,mult=%f", i, j, mult);
+        System.out.println(result);
+    }
+
+    private static void Task3() {
+        //TODO:
+    }
+
+    private static void Task4() {
+        //TODO:
+    }
+
+    private static void Task5() {
+        //TODO:
+    }
+}
